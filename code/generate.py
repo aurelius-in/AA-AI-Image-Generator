687cfca22efcf77d82ef11a2a31ef11c8927d87c
aurelius-in <89275354+aurelius-in@users.noreply.github.com>
2023-04-07 21:33:15 -0500

    consolidated portfolio assets

--- code/generate.py
+++ code/generate.py
@@ -1,22 +1,25 @@
-import torch
-import torchvision.utils as vutils
+import tensorflow as tf
+import numpy as np
+import PIL.Image
 
-# Loads the generator model that was previously trained and saved to a file called "aa_ai_image_generator.pt". 
+# Loads the generator model that was previously trained and saved to a file called "aa_ai_image_generator.pb". 
 # generates some random noise and passes it through the generator to generate a batch of 64 fake images. 
-# Finally, we save these generated images to a file called "generated_images.png" using the vutils.save_image() 
-# function from PyTorch's torchvision library.
-
-# Set device to run on GPU if available
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Finally, we save these generated images to a file called "generated_images.png" using the 
+# PIL.Image.save() function from Python's Pillow library.
 
 # Load generator model
-G = torch.load("aa_ai_image_generator.pt", map_location=device)
+model = tf.keras.models.load_model("aa_ai_image_generator.pb")
 
 # Generate random noise
-noise = torch.randn(64, 100, 1, 1, device=device)
+noise = tf.random.normal([64, 100])
 
 # Generate fake images
-fake_images = G(noise)
+fake_images = model.predict(noise)
+
+# Rescale pixel values to 0-255
+fake_images = (fake_images + 1) * 127.5
+fake_images = np.clip(fake_images, 0, 255).astype(np.uint8)
 
 # Save generated images to file
-vutils.save_image(fake_images.detach(), "generated_images.png", normalize=True, nrow=8)
+images_grid = PIL.Image.fromarray(tf.keras.preprocessing.image.array_to_img(fake_images, scale=False))
+images_grid.save("generated_images.png")
